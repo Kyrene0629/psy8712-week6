aa6f2081a051bbc5a0482332b658f4ed3996a8a0
Kyrene Li <li003631@umn.edu>
2026-03-01 22:05:19 -0600

    create a new tibble called citations_tbl

--- R/week6.R
+++ R/week6.R
@@ -14,3 +14,4 @@ str_c("The average number of characters/citation was", mean(str_length(citations
 # Data Cleaning
 citations_tbl %>%
   slice_sample(n = 20)
+citations_tbl <- tibble(line = seq_along(citations_txt), cite = citations_txt) %>%
